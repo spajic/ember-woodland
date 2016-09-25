9c6f09c49a22f7b94a80393c3918847c88d643ac
Alexey Vasilyev <bestspajic@gmail.com>
2016-09-25 21:49:26 +0300

    Add product, order, order-item models

--- app/models/order-item.js
+++ app/models/order-item.js
@@ -1,6 +1,6 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-  title: DS.attr('string'),
-  price:  DS.attr('number')
+  order: DS.belongsTo('order'),
+  quantity: DS.attr('number')
 });
--- app/models/order.js
+++ app/models/order.js
@@ -1,6 +1,5 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-  title: DS.attr('string'),
-  price:  DS.attr('number')
+  items: DS.hasMany('order-item')
 });
